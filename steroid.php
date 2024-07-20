c2742f7756f30b112101c17c81159d22bdb8a969
abishek3032 <abishekyo22@gmail.com>
2024-07-20 22:16:59 +0530

    Add files via upload

---
+++ steroid.php
@@ -0,0 +1,14 @@
+<?php
+function get(){
+    echo "GET REQUEST";
+}
+function post(){
+    echo "POST REQUEST";
+}
+function delete(){
+    echo "DELETE REQUEST";
+}
+function put(){
+    echo "UPDATE REQUEST";
+}
+steroid 1.0
